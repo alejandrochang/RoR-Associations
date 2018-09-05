dfe2344e99c4905f10fe76c1fe843ffb75d6fa89
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-05 15:30:45 -0700

    Make changes to association_rails after TA guidance

--- W3D3/associations_rails/app/models/course.rb
+++ W3D3/associations_rails/app/models/course.rb
@@ -1,24 +1,25 @@
 class Course < ApplicationRecord
-  
+  has_many :enrolled_students,
+  through: :enrollments, # one that exist in current class
+  source: :student # what you acces wherever that another association is at
+
   has_many :enrollments,
     primary_key: :id,
     foreign_key: :course_id,
     class_name: "Enrollment"
-    
+
   belongs_to :prerequisite,
     primary_key: :id,
+    optional: true,
     foreign_key: :prereq_id,
-    class_name: "Prerequisite"
-    
-  belongs_to :instructors,
+    class_name: "Course"
+
+  belongs_to :instructor,
     primary_key: :id,
     foreign_key: :instructor_id,
-    class_name: "Instructor"
-  
-  has_many :enrolled_students,
-   through: :enrollments, # one that exist in current class
-   source: :student # what you acces wherever that another association is at 
-   
+    class_name: "User"
+
+
 end
 
 # t.string "name"
