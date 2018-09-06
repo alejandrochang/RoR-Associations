53e7e643c2a1a8b4f2f64bf1db4c60ee26afbf3b
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-05 17:00:59 -0700

    Working on shortening url

--- W3D3/associations_rails/app/models/course.rb
+++ W3D3/associations_rails/app/models/course.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint(8)        not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Course < ApplicationRecord
   has_many :enrolled_students,
   through: :enrollments, # one that exist in current class
